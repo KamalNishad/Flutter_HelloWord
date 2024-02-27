1f11eb2c89853a38a565b68265dd1bd8deca0a4d
kamal_nishad <kamalnishad456@gmail.com>
2024-02-27 12:49:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //The runApp() function takes the given Widget and makes it the root of the widget tree.
+  runApp(
+    const Center(child: Text('Hello Text',textDirection: TextDirection.ltr),
+    ),
+  );
+}
+
